91ecc5ed52eeff2b8fed667fdd6e4706d3ed6871
alexis <alexis.bradstreet01@gmail.com>
2021-01-20 18:41:03 -0500

    js to scala day 1

---
+++ Day1.scala
@@ -0,0 +1,36 @@
+object Day1 extends App {
+  import scala.io.Source
+  val input = Source.fromFile("Input/Day1.txt")
+  val lines = try input.mkString finally input.close()
+  val nums = lines.split("\n").map(n => n.toInt)
+
+  //this method returns the product of the two entries that sum to 2020
+  def part1(arr:Array[Int]):Int = {
+    var answer:Int = 0
+    for (i <- 0 to arr.length-1) {
+      for (k <- 0 to arr.length-1) {
+        if (arr(i)+arr(k)==2020) {
+          answer = arr(i)*arr(k)
+        }
+      }
+    }
+    answer
+  }
+
+  def part2(arr:Array[Int]):Int = {
+    var answer:Int = 0
+    for (i <- 0 to arr.length-1) {
+      for (k <- 0 to arr.length-1) {
+        for (j <- 0 to arr.length-1) {
+          if (arr(i)+arr(k)+arr(j)==2020) {
+          answer = arr(i)*arr(k)*arr(j)
+          }
+        }
+      }
+    }
+    answer
+  }
+
+  println(part1(nums))
+  println(part2(nums))
+}
